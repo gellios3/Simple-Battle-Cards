5724651f2c62cdace1c05f9b1c635458cd733e01
Gellios3 <thegellios@gmail.com>
2018-06-06 18:04:35 +0300

    init

---
+++ Assets/Scripts/Signals/HitCartsSignal.cs
@@ -0,0 +1,11 @@
+﻿using Models;
+using Models.Arena;
+using Models.ScriptableObjects;
+using strange.extensions.signal.impl;
+
+namespace Signals.Arena
+{
+    public class HitCartsSignal : Signal<Card, BattleState>
+    {
+    }
+}
